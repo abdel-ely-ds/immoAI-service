a596ffd626b367a6f5d997147699fa7c438cad2c
abdel-ely-ds <abdel.ely.ds@gmail.com>
2023-06-10 14:18:32 +0200

    chat route added

--- app.py
+++ app.py
@@ -1,27 +1,62 @@
+import json
+import os
+from pathlib import Path
 from typing import Optional
 
 from fastapi import FastAPI, Depends, Query
 import pandas as pd
+from langchain import OpenAI
+from langchain.agents import create_pandas_dataframe_agent
 
+from data_utils import read, pre_process, cols
 from services.dashboard_service import DashboardService
 
 app = FastAPI()
 df: Optional[pd.DataFrame] = None
+llm = None
+agent = None
+
+
+def _set_key():
+    path = Path(".creds/.openai.json")
+    with open(path) as f:
+        creds = json.load(f)
+
+    os.environ["OPENAI_API_KEY"] = creds["OPENAI_API_KEY"]
 
 
 @app.on_event("startup")
 async def startup_event():
     global df
-    df = pd.read_csv("data.csv")
+    global llm
+    global agent
+
+    _set_key()
+    df = read("immo_data.json")
+    df = pre_process(df)[cols]
+    llm = OpenAI(temperature=0)
+    agent = create_pandas_dataframe_agent(llm, df, verbose=True)
 
 
-@app.get('/')
+@app.get("/")
 def root():
     return {"message": "working"}
 
 
+@app.get("/chat", status_code=200)
+async def chat(
+    question: str = Query(
+        "What is the average price in oulfa district ? Give a response as a whole number",
+        description="Ask any question about real estate market",
+    )
+):
+    return agent.run(question.lower().strip())
+
+
 @app.get("/dashboard", status_code=200)
-async def get_dashboard(country: str = Query("Country3", description="Country you are interested in"),
-                        city: str = Query("City5", description="City you are interested in"),
-                        service: DashboardService = Depends()):
+async def get_dashboard(
+    country: str = Query("Country3", description="Country you are interested in"),
+    city: str = Query("City5", description="City you are interested in"),
+    service: DashboardService = Depends(),
+):
     return service.overview(df, country=country, city=city)
---
+++ data_utils.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import json
+import re
+
+cols = ["price", "rooms", "surface", "city", "district"]
+
+
+def read(path: str) -> pd.DataFrame:
+    data = []
+    with open(path, "r", encoding="utf8") as file:
+        for line in file:
+            data.append(json.loads(line))
+
+    return pd.DataFrame.from_records(data)
+
+
+def _room_and_size(s, index=0, sep1=",", sep2=" "):
+    if s is None:
+        return
+
+    try:
+        return int(s.strip().split(sep1)[index].strip().split(sep2)[0])
+    except IndexError:
+        return
+
+
+def _price(p):
+    num_p = re.sub(r'\D', '', p.replace("\xa0", ""))
+    if not num_p:
+        return
+    return int(num_p)
+
+
+def pre_process(data: pd.DataFrame) -> pd.DataFrame:
+    data.price = data.price.apply(_price)
+    data["rooms"] = data.size_and_rooms.apply(lambda x: _room_and_size(x))
+    data["surface"] = data.size_and_rooms.apply(lambda x: _room_and_size(x, index=1))
+    data["city"] = data.location.apply(lambda x: x.split("à")[-1])
+    data["district"] = data.location.apply(lambda x: x.split("à")[0])
+    data.title = data.title.str.lower().str.strip()
+    data.city = data.city.str.lower().str.strip()
+    data.district = data.district.str.lower().str.strip()
+
+    data.drop(columns=["location"], inplace=True)
+    return data
