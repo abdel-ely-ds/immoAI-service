f771992bfe2b5e1681c6bc0746be9d0ec3d31f37
abdel-ely-ds <abdel.ely.ds@gmail.com>
2023-06-11 19:33:01 +0200

    rewrite

--- app.py
+++ app.py
@@ -1,41 +1,44 @@
-import json
-import os
-from pathlib import Path
 from typing import Optional
 
-from fastapi import FastAPI, Depends, Query
 import pandas as pd
+from fastapi import Depends, FastAPI, Query
 from langchain import OpenAI
-from langchain.agents import create_pandas_dataframe_agent
+from langchain.agents import initialize_agent
 from langchain.agents.agent import AgentExecutor
-from data_utils import read, pre_process, cols
+from langchain.memory import ConversationBufferMemory
+
+import data_utils as du
+import keys
+import tools
 from services.dashboard_service import DashboardService
 
 app = FastAPI()
 df: Optional[pd.DataFrame] = None
-llm: Optional[OpenAI] = None
 agent: Optional[AgentExecutor] = None
+memory = ConversationBufferMemory(memory_key="chat_history")
 
-
-def _set_key():
-    path = Path(".creds/.openai.json")
-    with open(path) as f:
-        creds = json.load(f)
-
-    os.environ["OPENAI_API_KEY"] = creds["OPENAI_API_KEY"]
+cols = ["price", "rooms", "surface", "city", "district", "description"]
 
 
 @app.on_event("startup")
 async def startup_event():
     global df
-    global llm
     global agent
 
-    _set_key()
-    df = read("immo_data.json")
-    df = pre_process(df)[cols]
+    keys.export()
+
+    df = du.read("data/immo_data.json")
+    df = du.pre_process(df)[cols]
     llm = OpenAI(temperature=0)
-    agent = create_pandas_dataframe_agent(llm, df, verbose=True)
+
+    agent = initialize_agent(
+        agent="conversational-react-description",
+        tools=tools.load(llm, df),
+        llm=llm,
+        verbose=True,
+        max_iterations=3,
+        memory=memory,
+    )
 
 
 @app.get("/")
@@ -43,7 +46,7 @@ def root():
     return {"message": "working"}
 
 
-@app.get("/chat", status_code=200)
+@app.get("/immo", status_code=200)
 async def chat(
     question: str = Query(
         "What is the average price in oulfa district ? Give a response as a whole number",
