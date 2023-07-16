4115b179ea97c9b5a09022bc805acf81a9557652
abdel-ely-ds <abdel.ely.ds@gmail.com>
2023-07-16 21:30:15 +0200

    wip

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source .aws_env
+# source .aws_env
 
 docker tag "${IMAGE_NAME}:${TAG}" "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPOSITORY}:${TAG}"
-aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr."$AWS_REGION".amazonaws.com
-docker push "$AWS_ACCOUNT_ID".dkr.ecr."$AWS_REGION".amazonaws.com/$ECR_REPOSITORY:$TAG
+aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID".dkr.ecr."$AWS_REGION".amazonaws.com
+docker push "$AWS_ACCOUNT_ID".dkr.ecr."$AWS_REGION".amazonaws.com/"$ECR_REPOSITORY":"$TAG"
