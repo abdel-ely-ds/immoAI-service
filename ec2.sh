67b7b536e5fb56b3e1dc2e5fa19ea224184027fc
abdel-ely-ds <abdel.ely.ds@gmail.com>
2023-07-17 21:26:01 +0200

    wip

--- ec2.sh
+++ ec2.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-#source .aws_env
+# source .aws_env
 
-INSTANCE_INFO=$(aws ec2 run-instances \
-  --image-id "$AMI_ID" \
-  --instance-type "$INSTANCE_TYPE" \
-  --region "$AWS_REGION" \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$APP_NAME}]" \
-  --output json
+EXISTING_INSTANCE=$(aws ec2 describe-instances \
+  --filters "Name=tag:Name,Values=$APP_NAME" "Name=instance-state-name,Values=running" \
+  --query 'Reservations[0].Instances[0].InstanceId' \
+  --output text
 )
 
+if [ -z "$EXISTING_INSTANCE" ] || [ "$EXISTING_INSTANCE" = "None" ]; then
+  INSTANCE_INFO=$(aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type "$INSTANCE_TYPE" \
+    --region "$AWS_REGION" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$APP_NAME}]" \
+    --output json
+  )
 
-INSTANCE_ID=$(echo "$INSTANCE_INFO" | grep -o '"InstanceId": "[^"]*' | awk -F ': "' '{print $2}')
+  INSTANCE_ID=$(echo "$INSTANCE_INFO" | grep -o '"InstanceId": "[^"]*' | awk -F ': "' '{print $2}')
 
-aws ec2 wait instance-running --instance-ids "$INSTANCE_ID" --region "$AWS_REGION"
+  aws ec2 wait instance-running --instance-ids "$INSTANCE_ID" --region "$AWS_REGION"
 
-PUBLIC_IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text --region "$AWS_REGION")
+  PUBLIC_IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text --region "$AWS_REGION")
 
-# Output the instance details
-echo "EC2 instance created successfully!"
-echo "Instance ID: $INSTANCE_ID"
-echo "Public IP: $PUBLIC_IP"
+  echo "EC2 instance created successfully!"
+  echo "Instance ID: $INSTANCE_ID"
+  echo "Public IP: $PUBLIC_IP"
+
+else
+  INSTANCE_ID="$EXISTING_INSTANCE"
+  PUBLIC_IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text --region "$AWS_REGION")
+
+  echo "An EC2 instance with Name=$APP_NAME is already running!"
+  echo "Instance ID: $INSTANCE_ID"
+  echo "Public IP: $PUBLIC_IP"
+fi
